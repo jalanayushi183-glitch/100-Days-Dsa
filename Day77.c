f1ec612c3f7d27de879f6129cb3d96fdadf20ab1
jalanayushi183-glitch <jalanayushi183@gmail.com>
2026-04-12 10:00:00 +0530

    seventy seven commit

---
+++ Day77.c
@@ -0,0 +1,62 @@
+// Problem Statement
+// Using BFS or DFS, check if the entire graph is connected.
+
+// Input Format
+// n m
+// edges
+
+// Output Format
+// CONNECTED
+// NOT CONNECTED
+
+// Sample Input
+// 4 2
+// 1 2
+// 3 4
+
+// Sample Output
+// NOT CONNECTED
+#include <stdio.h>
+
+int visited[1000];
+int adj[1000][1000];
+int n, m;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for(int i = 1; i <= n; i++) {
+        if(adj[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    dfs(1);
+
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) {
+            printf("NOT CONNECTED");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED");
+    return 0;
+}
